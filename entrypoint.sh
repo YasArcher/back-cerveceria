90716d6f8eb9c67bbada3ea3a78224eca550d544
Marlon Masabanda <marlon25.masabanda@gmail.com>
2025-07-21 20:21:38 -0500

    Cambio de orden en la ejecucion de datos

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ python manage.py migrate --noinput
 echo "🎯 Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
 
-echo "🚀 Iniciando Gunicorn..."
-exec gunicorn miapp.wsgi:application --bind 0.0.0.0:8000
-
 echo "📥 Insertando datos iniciales desde SQL..."
 python manage.py dbshell < init.sql
+
+echo "🚀 Iniciando Gunicorn..."
+exec gunicorn miapp.wsgi:application --bind 0.0.0.0:8000
